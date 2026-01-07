674f0226f933b80e155e1dcb4f67a9647121eef4
tuyet22 <caothianhtuyet221@gmail.com>
2026-01-07 17:30:53 +0700

    first commit

---
+++ hello.ts
@@ -0,0 +1,8 @@
+let message: string = "Chào bạn! Đây là TypeScript.";
+let year: number = 2024;
+
+function sayHello(name: string): string {
+    return `Hello ${name}, chào mừng bạn đến với năm ${year}!`;
+}
+
+console.log(sayHello("Học viên"));
